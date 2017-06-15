30e90db867aaceb71bda5f1fac56b877348a1878
Robert Puls <rpu1s@sbcglobal.net>
2017-06-15 15:04:45 -0600

    updates default route

--- app.js
+++ app.js
@@ -3,18 +3,20 @@ const bodyParser = require('body-parser');
 const weeds = require("./routes/weedRoutes");
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(bodyParser.json());
 
-app.use("/",weeds);
+app.use("/weeds", weeds);
 
-app.use(function (req, res, next){
+app.use(function(req, res, next) {
   var err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
 
-app.use(function (err, req, res, next){
+app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.json({
     "message": err.message,
@@ -24,5 +26,7 @@ app.use(function (err, req, res, next){
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log("ready");
+console.log("ready");
+});
+
 });
